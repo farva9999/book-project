103d9f81e75f71124637601319662683d1aad819
farva9999 <decofinde@gmail.com>
2022-08-21 21:11:44 -0400

    completed phase1

--- index.js
+++ index.js
@@ -20,33 +20,7 @@ function addBookToLibrary() {
     //add to library array
     const addedBook = new Book(title, author, readstatus);
 
-    //create div to hold book elements
-    let newBookGridElement = document.createElement('div');
-    newBookGridElement.className = 'book';
-
-    //set data info on element
-    newBookGridElement.dataset.info = `${author}${title}${readstatus}`;
-
-    //create p elements and button
-    let newBookTitle = document.createElement("p");
-        newBookTitle.textContent = addedBook.title;
-    let newBookAuthor = document.createElement("p");
-        newBookAuthor.textContent = addedBook.author;
-    let newBookReadStatus = document.createElement("p");
-        newBookReadStatus.textContent = addedBook.readstatus;
-
-    let deleteButton = document.createElement('button');
-        deleteButton.textContent = "Delete";
-        deleteButton.dataset.info = `${author}${title}${readstatus}`;
-      deleteButton.addEventListener('click', deleteBook);
-
-    libraryDisplay.appendChild(newBookGridElement);
-    newBookGridElement.appendChild(newBookTitle);
-    newBookGridElement.appendChild(newBookAuthor);
-    newBookGridElement.appendChild(newBookReadStatus);
-    newBookGridElement.appendChild(deleteButton);
-
-    myLibrary.push(addedBook);
+    
 
     console.log(myLibrary);
 
@@ -75,7 +49,8 @@ function eraseLibrary() {
 
 //THE BIG CONSTRUCTOR
 function Book(title, author, readstatusin) {
-
+  let bookID = Date.now()
+    this.ID = bookID;
     this.title = title;
     this.author = author;
     if (readstatusin == true) {
@@ -83,6 +58,46 @@ function Book(title, author, readstatusin) {
     } else {
         this.readstatus = "Not Read";
     }
+
+    //create div to hold book elements
+    let newBookGridElement = document.createElement('div');
+    newBookGridElement.className = 'book';
+
+    //set data info on element
+    newBookGridElement.dataset.info = `${author}${title}${readstatus}`;
+
+    //create p elements and button
+    let newBookTitle = document.createElement("p");
+        newBookTitle.textContent = this.title;
+    let newBookAuthor = document.createElement("p");
+        newBookAuthor.textContent = this.author;
+    let newBookReadStatus = document.createElement("p");
+        newBookReadStatus.textContent = this.readstatus;
+        newBookReadStatus.id = bookID;
+
+        //create delete button
+    let deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
+        deleteButton.dataset.info = `${author}${title}${readstatus}`;
+        deleteButton.addEventListener('click', deleteBook);
+
+      //create toggle read button
+      let readToggle = document.createElement('button');
+        readToggle.textContent = "Toggle Read";
+        readToggle.dataset.author = `${author}`;
+        readToggle.dataset.title = `${title}`;
+        readToggle.dataset.readStatus = `${readstatus}`;
+        readToggle.dataset.ID = this.ID;
+        readToggle.addEventListener('click', toggleRead);
+
+    libraryDisplay.appendChild(newBookGridElement);
+    newBookGridElement.appendChild(newBookTitle);
+    newBookGridElement.appendChild(newBookAuthor);
+    newBookGridElement.appendChild(newBookReadStatus);
+    newBookGridElement.appendChild(deleteButton);
+    newBookGridElement.appendChild(readToggle);
+
+    myLibrary.push(this);
 
 }
 
@@ -100,5 +115,18 @@ function deleteBook(e){
   let buttonID = e.target.dataset.info;
   let itemsToDelete = document.querySelectorAll(`[data-info="${buttonID}"]`);
   itemsToDelete.forEach(element => element.remove());
-
 }
+  
+  function toggleRead(e){
+    let buttonID = e.target.dataset.ID;
+    let objectToToggle = myLibrary.find(x => x.ID = buttonID);
+    if (objectToToggle.readstatus == 'Read') {
+      objectToToggle.readstatus = 'Not Read'
+      document.getElementById(buttonID).textContent = 'Not Read';
+    }else if (objectToToggle.readstatus == 'Not Read') {
+      objectToToggle.readstatus = 'Read'
+      document.getElementById(buttonID).textContent = 'Read';
+    }
+
+    console.log(objectToToggle);
+  }
